df9c2ca3a653f41f7a49ec75ab98667b123fc5e2
paoloferrari97 <ferrari.paolo.97@gmail.com>
2021-06-08 11:39:47 +0200

    Salvo in data i risultati

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,8 @@ const app = new Vue({
     el: "#app",
     data: {
         url: "https://api.themoviedb.org/3/search/movie?api_key=601ef4a8959c32c43648a3337f0ccbfd&query=",
-        input: ""
+        input: "",
+        movies: []
     },
     methods: {
         cerca() {
@@ -10,7 +11,8 @@ const app = new Vue({
             axios
             .get(this.url)
             .then(resp => {
-                console.log(resp);    
+                console.log(resp);
+                this.movies = resp;
             })
             .catch(e => {
                 console.log(e);    
